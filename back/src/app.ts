8787c52c74eee4dd9f005077677d44afaa38d9c8
adelnizamutdinov <stiggpwnz@gmail.com>
2021-05-10 12:49:52 +0300

    Ws server

---
+++ back/src/app.ts
@@ -0,0 +1,24 @@
+import restana from "restana";
+import http from "http";
+import WebSocket from 'ws'
+
+const app = restana()
+  .get("/", (req, res) => {
+    res.end("foo")
+  })
+
+const PORT = 4000
+
+// @ts-ignore restana types
+const server = http.createServer(app)
+
+const wsServer = new WebSocket.Server({ server })
+
+wsServer.on('connection', ws => {
+
+})
+
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`)
+})
+
